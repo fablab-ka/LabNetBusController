ce0e8468bcd2bfe572e9a2a6e4444008ec5e2c9f
Sven Hecht <sven.hecht@citrix.com>
2016-04-06 20:43:22 +0200

    fixed develop.sh script

--- develop.sh
+++ develop.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
+current_dir=$(pwd)
+echo "${current_dir}"
+
 docker build -t labnet-buscontroller .
-docker run --rm -it -v /src:./src --name labnet-buscontroller-1 labnet-buscontroller
+docker run --rm -it -v /src:"${current_dir}/src" --name labnet-buscontroller-1 labnet-buscontroller
--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 docker build -t labnet-buscontroller .
 docker run --rm -it --name labnet-buscontroller-1 labnet-buscontroller
